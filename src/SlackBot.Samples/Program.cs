a5b23a484edd07a7c8b4695405dcc1c5d6d160a1
Sergey Vasnin <sergey.vasnin@fuse8.ru>
2020-09-01 14:33:24 +0500

    removed usage of IHost

--- src/SlackBot.Samples/Program.cs
+++ src/SlackBot.Samples/Program.cs
@@ -1,53 +1,33 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using SlackBot.Api;
 using SlackBot.Api.Models;
-using Microsoft.Extensions.Hosting;
 using SlackBot.Samples.Configurations;
 using SlackBot.Samples.Extensions;
 
 namespace SlackBot.Samples
 {
-    public class Program
-    {
-        const string EnvironmentVariableName = "ASPNETCORE_ENVIRONMENT";
-        
-        public static async Task Main(string[] args)
-        {
-            var host = BuildHost(args);
-            
-            using var scope = host.Services.CreateScope();
-            var serviceProvider = scope.ServiceProvider;
-            
-            var configuration = serviceProvider.GetService<IConfiguration>();
-            var slackBotSettings = configuration.GetSection<SlackBotSettings>("SlackBotSettings");
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var configuration = GetConfiguration();
+			var slackBotSettings = configuration.GetSection<SlackBotSettings>("SlackBotSettings");
 
-            var slackClient = new SlackClient(slackBotSettings.Token);
-            var message = new Message
-            {
-                Channel = "slack-bot-api-test",
-                Text = "some message"
-            };
-            
-            var postMessage = await slackClient.PostMessage(message);
-        }
-        
-        private static IHost BuildHost(string[] args)
-        {
-            var hostBuilder = Host.CreateDefaultBuilder(args);
+			var slackClient = new SlackClient(slackBotSettings.Token);
+			var message = new Message
+			{
+				Channel = "slack-bot-api-test",
+				Text = "some message"
+			};
 
-            var environment = ParseEnvironment();
-            if (!string.IsNullOrEmpty(environment))
-            {
-                hostBuilder.UseEnvironment(environment);
-            }
-            
-            return hostBuilder.Build();
-        }
+			var postMessage = await slackClient.PostMessage(message);
+		}
 
-        private static string ParseEnvironment() 
-            => Environment.GetEnvironmentVariable(EnvironmentVariableName);
-    }
+		private static IConfiguration GetConfiguration() =>
+			new ConfigurationBuilder()
+				.AddJsonFile("appsettings.json")
+				.AddJsonFile("appsettings.Development.json", optional: true)
+				.Build();
+	}
 }
